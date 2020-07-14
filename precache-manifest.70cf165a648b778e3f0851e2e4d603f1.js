1d05705f748d65511601b956d981e22d931864fa
Maksym Maiboroda <crashdey323@gmail.com>
2020-07-14 10:24:21 +0300

    Updates

---
+++ precache-manifest.70cf165a648b778e3f0851e2e4d603f1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c58f082410ba897a8dff688dec5473c",
+    "url": "/test_task/index.html"
+  },
+  {
+    "revision": "3fae93f7166897c29dec",
+    "url": "/test_task/static/css/2.9edc93b8.chunk.css"
+  },
+  {
+    "revision": "9b80187f3ec62af01450",
+    "url": "/test_task/static/css/main.9bcd6cab.chunk.css"
+  },
+  {
+    "revision": "3fae93f7166897c29dec",
+    "url": "/test_task/static/js/2.04a356f2.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/test_task/static/js/2.04a356f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b80187f3ec62af01450",
+    "url": "/test_task/static/js/main.a4f72115.chunk.js"
+  },
+  {
+    "revision": "a155c0f7252e842757fc",
+    "url": "/test_task/static/js/runtime-main.2c49d933.js"
+  },
+  {
+    "revision": "bb86db3b92f201c6dca32ebe9b147e48",
+    "url": "/test_task/static/media/background.bb86db3b.png"
+  }
+]);
